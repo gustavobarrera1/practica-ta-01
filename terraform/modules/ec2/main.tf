a3ce6209025150d9045b78cca1894a96fadf0d74
Gustavo Barrera <gustavobarrera1@outlook.es>
2026-06-01 19:16:37 -0300

    Testing deploy app with SSM in GitHub Actions

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "ec2" {
 
   tags = {
     Name = "tf-lab-01"
+    Environment = var.env
   }
 
   user_data = file("${path.module}/../../install_docker.sh")
@@ -31,8 +32,8 @@ resource "aws_ssm_document" "deploy_app" {
         name   = "deploy"
 
         inputs = {
-          runCommand = [
-            "set -e", 
+          runCommand = [ 
+            "set -e", # Esto está acá porque puede ser el caso que no este ssm totalmente disponible. En vez de hacerlo mediante el script de la vm. Se tiene que revisar para ver de poder quitar está sección.
             "mkdir -p /root/.ssh",
             "chmod 700 /root/.ssh",
             "aws ssm get-parameter --name /flaskapp/${var.env}/github/deploy_key --with-decryption --query Parameter.Value --output text > /root/.ssh/id_ed25519",
@@ -42,16 +43,7 @@ resource "aws_ssm_document" "deploy_app" {
             "cd /opt/dev",
             
             "if [ ! -d /opt/dev/.git ]; then git clone git@github.com:gustavobarrera1/practica-ta-01.git .; fi",
-            "git pull origin main",          
-            "cd /opt/dev/app",
-
-            "echo \"DB_HOST=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_host --with-decryption --query Parameter.Value --output text)\" > .env",
-            "echo \"DB_NAME=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_name --with-decryption --query Parameter.Value --output text)\" >> .env",
-            "echo \"DB_USERNAME=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_username --with-decryption --query Parameter.Value --output text)\" >> .env",
-            "echo \"DB_PASSWORD=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_password --with-decryption --query Parameter.Value --output text)\" >> .env",
-            
-            "docker compose -f docker-compose-aws.yaml down || true",
-            "docker compose -f docker-compose-aws.yaml up -d"
+            "git pull origin main",
           ]
         }
       }
@@ -70,4 +62,37 @@ resource "aws_ssm_association" "deploy_app_assoc" {
 
   
   depends_on = [ aws_ssm_document.deploy_app, time_sleep.wait_for_instance_setup ] 
+}
+
+resource "aws_ssm_document" "deploy_app_github" {
+  name          = "DeployAppGithub"
+  document_type = "Command"
+
+  content = jsonencode({
+    schemaVersion = "2.2"
+    description   = "Deploy application with GitHub"
+
+    mainSteps = [
+      {
+        action = "aws:runShellScript"
+        name   = "deploy"
+
+        inputs = {
+          runCommand = [
+            "set -e",        
+            "cd /opt/dev/app",
+
+            "echo \"DB_HOST=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_host --with-decryption --query Parameter.Value --output text)\" > .env",
+            "echo \"DB_NAME=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_name --with-decryption --query Parameter.Value --output text)\" >> .env",
+            "echo \"DB_USERNAME=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_username --with-decryption --query Parameter.Value --output text)\" >> .env",
+            "echo \"DB_PASSWORD=$(aws ssm get-parameter --name /flaskapp/${var.env}/database/db_password --with-decryption --query Parameter.Value --output text)\" >> .env",
+            
+            "docker compose -f docker-compose-aws.yaml down || true",
+            "docker compose -f docker-compose-aws.yaml up -d"
+          ]
+        }
+      }
+    ]
+  })
+  depends_on = [ aws_instance.ec2, aws_ssm_document.deploy_app ]
 }
